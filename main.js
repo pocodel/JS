05c9c803501883f320b1b95fe26727615daff0ad
pocodel <toppoptop228@gmail.com>
2025-03-05 22:49:22 +0300

    () =>

--- main.js
+++ main.js
@@ -1,49 +1,22 @@
 // hello!
 
-function checkAge(age){
-    return (age > 18) ? true : confirm('sooooo');
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   ask(
+//     "Вы согласны?",
+//     function() { alert("Вы согласились."); },
+//     function() { alert("Вы отменили выполнение."); }
+//   );
+
+let ask = (question, yes, no) => {
+    return (confirm(question)) ? yes() : no();
 }
 
-alert(checkAge(19));
-
-
-
-function checkAge(age){
-    return (age > 18) || confirm('fff');
-}
-
-alert(checkAge(19));
-
-
-
-function min(a, b){
-    if (a < b) {
-        return a;
-    } else {
-        return b;
-    }
-}
-
-alert(min(1, 2));
-
-
-// Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
-// P.S. В этой задаче функция обязана поддерживать только натуральные значения n, 
-// т.е. целые от 1 и выше.
-
-function pow(x, n) {
-
-    let result = x;
-    for (let i = 1; i < n; i++){
-        result *= x;
-    }
-    return result;
-}
-
-let x = prompt('enter x', '');
-let n = prompt('enter n', '');
-
-if (n >= 1 && n % 1 == 0){
-    alert(pow(x, n));
-} else alert('nope');
-
+ask(
+    "agree?",
+    () => alert('yes'),
+    () => alert('no')
+);
