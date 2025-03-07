f39ef92c1ce8580428294a6e846fd82360e4f6c5
pocodel <toppoptop228@gmail.com>
2025-03-08 00:11:45 +0300

    new

--- main.js
+++ main.js
@@ -1,64 +1,70 @@
-// hello!
+// // hello!
 
- let calculator = {
-    a: 0,
-    b: 0,
+// let obj = {};
 
+// function A() {
+//     return obj;
+// }
 
-    read() {
-        this.a = +prompt('a', 0);
-        this.b = +prompt('b', 0);
-    },
+// function B() {
+//     return obj;
+// }
 
-    sum() {
-        return this.a + this.b;
-    },
+// let a = new A();
+// let b = new B();
 
-    mul() {
-        return this.a * this.b;
-    }
- }
+// console.log(a == b);
 
-// *****************************************
-
- console.log(calculator);
-
- calculator.read();
-
- console.log(calculator.sum());
- console.log(calculator.mul());
-
- console.log(calculator);
-
-let ledder = {
-    step: 0,
-
-    showStep () {
-        alert(this.step);
-        return this;
-    },
-
-    up () {
-        this.step++;
-        return this;
-    },
-
-    down () {
-        this.step--;
-        return this;
-    },
-}
-
-ledder
-.showStep()
-.up()
-.showStep()
-.down()
-.showStep();
+// ++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+// Создайте функцию-конструктор Calculator, которая создаёт объекты с тремя методами:
+// read() запрашивает два значения при помощи prompt и сохраняет их значение в свойствах объекта.
+// sum() возвращает сумму этих свойств.
+// mul() возвращает произведение этих свойств.
+
+// function Calculator() {
+
+//     this.read = function() {
+//         this.a = +prompt('enter a', 0);
+//         this.b = +prompt('enter b', 0);
+//     }
+
+//     this.sum = function() {
+//         return this.a + this.b;
+//     }
+
+//     this.mul = function() {
+//         return this.a * this.b;
+//     }
+// }
+
+// let calculator = new Calculator();
+
+// calculator.read();
+
+// console.log(calculator.sum());
+// console.log(calculator.mul());
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Создайте функцию-конструктор Accumulator(startingValue).
+// Объект, который она создаёт, должен уметь следующее:
+// Хранить «текущее значение» в свойстве value. 
+// Начальное значение устанавливается в аргументе конструктора startingValue.
+// Метод read() должен использовать prompt для считывания нового числа и прибавления его к value.
 
 
+// function Accumulator (startingValue) {
+//     this.value = startingValue;
 
+//     this.read = function() {
+//         this.value += +prompt('enter value', 0);
+//     }
+// }
 
+// let accumulator = new Accumulator(1);
 
+// accumulator.read();
+
+// console.log(accumulator.value);
