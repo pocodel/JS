b247818586baf894ec638750f23c88897b1f6747
pocodel <toppoptop228@gmail.com>
2025-03-15 15:44:46 +0300

    array methods

--- main.js
+++ main.js
@@ -1,78 +1,244 @@
 // // hello!
 
-// Создайте массив styles с элементами «Джаз» и «Блюз».
-// Добавьте «Рок-н-ролл» в конец.
-// Замените значение в середине на «Классика». 
-// Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
-// Удалите первый элемент массива и покажите его.
-// Вставьте Рэп и Регги в начало массива.
+// Напишите функцию camelize(str), которая преобразует строки вида
+//  «my-short-string» в «myShortString».
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
 
-let arr = ['jazz', 'bluzz'];
-
-arr.push('rock');
-
-console.log(arr);
-
-arr[((arr.length - 1) / 2)] = 'class';
-console.log(arr);
-
-console.log(arr.shift());
-
-console.log(arr);
-
-arr.unshift('rap', 'reggi');
-
-console.log(arr);
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// Напишите функцию sumInput(), которая:
-// Просит пользователя ввести значения, используя prompt и сохраняет их в массив.
-// Заканчивает запрашивать значения, когда пользователь введёт не числовое значение, 
-// пустую строку или нажмёт «Отмена».
-// Подсчитывает и возвращает сумму элементов массива.
-// P.S. Ноль 0 – считается числом, не останавливайте ввод значений при вводе «0».
-
-let = sumInput = () => {
-    
-    let arr = [];
-    
-    while (true) {
-        let value = prompt('enter num', 0);
-        
-        if (value === null || value === '' || !isFinite(value)) break;
-        
-        arr.push(+value);
-    }
-
-    let count = 0;
-    
-    for (let arrs of arr) {
-        count += arrs;
-    }
-
-    console.log(arr);
-    console.log(count);
+let camelize = (str) => {
+   return str
+    .split('-')
+    .map((item, index) => 
+    (index == 0) ? item : item[0].toUpperCase() + item.slice(1)
+    )
+    .join('');
 }
 
-sumInput();
+console.log(camelize('bo-bob-dop'));
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-function getMaxSubSum(arr) {
-    let maxSum = 0;
-    let partialSum = 0;
-  
-    for (let item of arr) { 
-      partialSum += item; 
-      
-      maxSum = Math.max(maxSum, partialSum); 
+///////////////////////////////////////////////////////////////////////////////////////
 
-      if (partialSum < 0) partialSum = 0; 
+Напишите функцию filterRange(arr, a, b),
+которая принимает массив arr, ищет элементы со значениями больше или равными
+a и меньше или равными b и возвращает результат в виде массива.
+
+
+let numbers = [5, 3, 8, 1];
+
+let filteredRange = (arr, a, b) => {
+    return arr.filter(item => (a <= item && item <= b));
+}
+
+let filter = filteredRange(numbers, 1, 4);
+
+console.log(filter);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Напишите функцию filterRangeInPlace(arr, a, b), 
+// которая принимает массив arr и удаляет из него все значения кроме тех,   
+// которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+
+let numbers = [5, 3, 8, 1, 4, 2, 9];
+
+let filterRangePlace = (arr, a, b) => {
+    for (let i = 0; i < arr.length; i++) {
+       let value = arr[i];
+
+       if (value < a || value > b) {
+        arr.splice(i, 1);
+        i--;
+       }
     }
-  
-    return maxSum;
-  }
+}
 
-  alert( getMaxSubSum([-1, 2, 3, -9]) ); 
+filterRangePlace(numbers, 1, 4);
+
+alert(numbers);
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Сортировать в порядке по убыванию
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+alert(arr);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+// Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+
+let copySorted = (arr) => {
+    
+    return arr.slice().sort();
+}
+
+let sorted = copySorted(arr);
+
+alert(sorted);
+alert(arr);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+let Calculator = function(str){
+    this.methods = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+    }
+
+    this.calculate = function(str) {
+        let split = str.split(' ');
+        let a = +split[0];
+        let op = split[1];
+        let b = +split[2];
+        
+        if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        } return this.methods[op](a, b); 
+    }
+
+    this.addMethod = function(name, func) {
+        this.methods[name] = func;
+    };
+}
+
+let calc = new Calculator;
+
+calc.addMethod('*', (a, b) => a * b);
+let result = calc.calculate('3 * 3');
+alert(result);
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// У вас есть массив объектов user, 
+// и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name);
+
+alert((names));
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, 
+// где fullName – состоит из name и surname.
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let userMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id,
+}));
+
+alert(userMapped[1].fullName);
+alert(userMapped[1].id);
+
+console.log((userMapped));
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Напишите функцию sortByAge(users), 
+// которая принимает массив объектов со свойством age и сортирует их по нему.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let sortByAge = (arr) => {
+    arr.sort((a, b) => a.age - b.age);
+}
+
+sortByAge(users);
+console.log(users);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Напишите функцию getAverageAge(users),
+// которая принимает массив объектов со свойством age и возвращает средний возраст.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let users = [ vasya, petya, masha ];
+
+let averageAge = (arr) => {
+    return arr.reduce((sum, user) => sum + user.age, 0) / users.length;
+}
+
+console.log(averageAge(users));
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Пусть arr – массив строк.
+// Напишите функцию unique(arr), которая возвращает массив,
+//  содержащий только уникальные элементы arr.
+
+let strings = ["кришна", "кришна", "харе", "харе",
+    "харе", "харе", "кришна", "кришна", ":-O"];
+
+    let uniqe = (str) => {
+
+        let result = [];
+        
+        for (let st of str) {
+            if (!result.includes(st)) {
+                result.push(st);
+            }
+        }
+
+        return result;
+    }
+
+console.log(uniqe(strings));
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Допустим, мы получили массив пользователей в виде {id:..., name:..., age:... }.
+// Создайте функцию groupById(arr), 
+// которая создаст из него объект с id в качестве ключа и элементами массива в качестве значений.
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+
+  function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
+  
+  let usersById = groupById(users);
+
+  console.log(usersById);
+
+
+
+
+
+
+
 
