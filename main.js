9ef724b5842c17ee085144fe397e485bf0003f60
pocodel <toppoptop228@gmail.com>
2025-03-24 19:20:39 +0300

    recursion

--- main.js
+++ main.js
@@ -1,100 +1,49 @@
+// hello!
 
-let date = new Date(2012, 1, 20, 3, 12);
+// Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
 
-alert(date);
-
-
-/////////////////////////////////////////////////////////////////
-
-// Напишите функцию getWeekDay(date), показывающую день недели в коротком формате: 
-// «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».
-
-let arr = ['sun', 'mn', 'th', 'wh', 'tha', 'fr', 'sat'];
-
-let getWeekDay = (date) => {
-    return arr[date.getDay()]
-};
-
-date = new Date(2025, 2, 21);
-
-console.log(getWeekDay(date));
-
-
-/////////////////////////////////////////////////////////////////
-
-// Напишите функцию getLocalDay(date), которая возвращает «европейский» день недели для даты date.
-
-
-let getLocalDate = (date) => {
-    if (date.getDay() == 0) {
-        return 7;
-    } else return date.getDay();
-};
-
-let date = new Date(2025, 2, 21);
-
-console.log(getLocalDate(date));
-
-
-/////////////////////////////////////////////////////////////////
-
-// Создайте функцию getDateAgo(date, days), 
-// возвращающую число, которое было days дней назад от даты date.
-
-let date = new Date(2025, 2, 21);
-
-let getDateAgo = (date, day) => {
-    let copy = new Date(date);
-    copy.setDate(date.getDate() - day);
-
-    return copy;
-};
-
-console.log(getDateAgo(date, 1));
-
-/////////////////////////////////////////////////////////////////
-
-// Напишите функцию getLastDayOfMonth(year, month), возвращающую последнее число месяца. 
-// Иногда это 30, 31 или даже февральские 28/29.
-
-
-let getLastDayOfYear = (year, month) => {
-    let date = new Date(year, month + 1, 0);
-    return date.getDate();
-
+let toSum = (n) => {
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result;
 }
 
-console.log(getLastDayOfYear(2012, 1));
+alert(toSum(100));
 
-/////////////////////////////////////////////////////////////////
-
-// Напишите функцию getSecondsToday(), возвращающую количество секунд с начала сегодняшнего дня.
-
-let getSecondsToday = () => {
-    let hour = new Date();
-    hour = (hour.getHours() * 3600) + (hour.getMinutes() * 60) + hour.getSeconds();
-    alert(hour);
+let toSum = (n) => {
+    if (n < 1) {
+        return n + toSum(n - 1);
+    } else {
+        return 1;
+    }
 }
 
-getSecondsToday();
-
-/////////////////////////////////////////////////////////////////
-
-// Создайте функцию getSecondsToTomorrow(), возвращающую количество секунд до завтрашней даты.
-
-let getSecondsToTomorrow = () => {
-    let time = new Date();
-    let full = 86400;
-
-    return full - (time = (time.getHours() * 3600) + (time.getMinutes() * 60) + time.getSeconds());
-};
-
-console.log(getSecondsToTomorrow());
-
-/////////////////////////////////////////////////////////////////
+alert(toSum(3));
 
 
+let toSum = (n) => {
+    return n * (n + 1) / 2;
+}
+
+alert(toSum(4));
 
 
+/////////////////////////////////////////////////////////////////////////////////////
+
+// адача – написать функцию factorial(n), которая возвращает n!, используя рекурсию.
+
+let dac = (n) => {
+    if (n == 1) {
+        return 1;
+    } else {
+        return n * dac(n - 1);
+    }
+}
+
+alert(dac(5));
+
+/////////////////////////////////////////////////////////////////////////////////////
 
 
