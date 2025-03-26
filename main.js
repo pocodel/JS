b5a5a6df5127a2c2945affc4b1fe49d32a35fa4c
pocodel <toppoptop228@gmail.com>
2025-03-26 22:31:41 +0300

    замыкание

--- main.js
+++ main.js
@@ -1,49 +1,51 @@
 // hello!
 
-// Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+// inBetween(a, b) – между a и b (включительно).
+// inArray([...]) – находится в данном массиве.
 
-let toSum = (n) => {
-    let result = 0;
-    for (let i = 1; i <= n; i++) {
-        result += i;
-    }
-    return result;
-}
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
-alert(toSum(100));
-
-let toSum = (n) => {
-    if (n < 1) {
-        return n + toSum(n - 1);
-    } else {
-        return 1;
+let inBetween = (a, b) => {
+    return function(x) {
+        return x >= a && x <= b;
     }
 }
 
-alert(toSum(3));
+alert(arr.filter(inBetween(3,6)));
 
+ let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let toSum = (n) => {
-    return n * (n + 1) / 2;
-}
-
-alert(toSum(4));
-
-
-/////////////////////////////////////////////////////////////////////////////////////
-
-// адача – написать функцию factorial(n), которая возвращает n!, используя рекурсию.
-
-let dac = (n) => {
-    if (n == 1) {
-        return 1;
-    } else {
-        return n * dac(n - 1);
+ let inArray = (arr) => {
+    return function(x) {
+        return arr.includes(x);
     }
-}
-
-alert(dac(5));
-
-/////////////////////////////////////////////////////////////////////////////////////
+ }
 
 
+ alert(arr.filter(inArray([1, 2, 10])));
+
+
+/////////////////////////////////////////////////////////////
+
+function makeArmy() {
+    let shooters = [];
+  
+    let i = 0;
+    while (i < 10) {
+      let shooter = function() { // функция shooter
+        alert( i ); // должна выводить порядковый номер
+      };
+      shooters.push(shooter); // и добавлять стрелка в массив
+      i++;
+    }
+  
+    // ...а в конце вернуть массив из всех стрелков
+    return shooters;
+  }
+  
+  let army = makeArmy();
+
+  // все стрелки выводят 10 вместо их порядковых номеров (0, 1, 2, 3...)
+army[0](); // 10 от стрелка с порядковым номером 0
+army[1](); // 10 от стрелка с порядковым номером 1
+army[2](); // 10 ...и т.д.
