a78b017407f04061e602136f20993dac94bb6590
pocodel <toppoptop228@gmail.com>
2025-05-27 19:04:08 +0300

    style

--- main.js
+++ main.js
@@ -1,50 +1,37 @@
 // hello!
 
-class Clock {
-    constructor({ template }) {
-      this.template = template;
+
+ function showNotification({top = 0, right = 0, className, html}) {
+
+      let notification = document.createElement('div');
+      notification.className = 'notification';
+
+      if (className) {
+        notification.classList.add(className);
+      }
+
+      notification.style.top = top + 'px';
+      notification.style.right = right + 'px';
+      notification.innerHTML = html;
+
+      document.body.append(notification);
+
+      setTimeout(() => {
+        notification.remove(className);
+      }, 1500);
+
     }
-  
-    render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = this.template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    }
-  
-    stop() {
-      clearInterval(this.timer);
-    }
-  
-    start() {
-      this.render();
-      this.timer = setInterval(() => this.render(), 1000);
-    }
-  }
 
 
-  class ExtendClock extends Clock {
-    constructor(precision) {
-        this.precision = precision;
-    }
-  }
-
-let clock = new Clock({template: 'h:m:s'});
-
-let extendClock = new Clock(1000);
+let i = 1;
+    setInterval(() => {
+      showNotification({
+        top: 10,
+        right: 10,
+        html: 'Hello ' + i++,
+        className: "welcome"
+      });
+    }, 2000);
 
 
 
